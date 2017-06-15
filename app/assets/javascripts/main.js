d8ee92ee3fbb1c115ec7c063c5aa6c3d9e301cc1
Mike Fogg <me@mikefogg.co>
2017-06-15 17:52:48 -0400

    Fix some spacing and removing turbo links cause it sucks

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,50 +1,43 @@
 $(document).ready(function(){
+	//Get the modal
+	var modal = document.getElementById('modal');
 
+	// Get the button that opens the modal
+	var $btn = document.getElementsByClassName("modalBtn");
 
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
 
-    //Get the modal
-  var modal = document.getElementById('modal');
-
-  // Get the button that opens the modal
-  var $btn = document.getElementsByClassName("modalBtn");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on the button, open the modal
-  $('.modal-btn').click( function(){
-      modal.style.display = "block";
-  });
-
-  // When the user clicks on <span> (x), close the modal
-  $('.close').on('click', function(){
-      modal.style.display = "none";
-  });
-
-  // When the user clicks anywhere outside of the modal, close it
-  $(window).on('click', function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  });
-  $('.create-project').on('click', function(){
-  		$.getScript('/projects/new');
-      // $('#modal').show();
-  		// Do this at the end so it doesn't actually click the link!
-  		return false;
-      // var modal = document.getElementById('modal');
+	// When the user clicks on the button, open the modal
+	$('.modal-btn').click( function(){
+		 modal.style.display = "block";
 	});
-  $('.create-step').on('click', function(){
-  		$.getScript('/steps/new');
-      // $('#modal').show();
-  		// Do this at the end so it doesn't actually click the link!
-  		return false;
-      // var modal = document.getElementById('modal');
 
-  });
+	// When the user clicks on <span> (x), close the modal
+	$('.close').on('click', function(){
+		 modal.style.display = "none";
+	});
 
-
-
+	// When the user clicks anywhere outside of the modal, close it
+	$(window).on('click', function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	});
+	$('.create-project').on('click', function(){
+		$.getScript('/projects/new');
+		// $('#modal').show();
+		// Do this at the end so it doesn't actually click the link!
+		return false;
+		// var modal = document.getElementById('modal');
+	});
+	$('.create-step').on('click', function(){
+		$.getScript('/steps/new');
+		// $('#modal').show();
+		// Do this at the end so it doesn't actually click the link!
+		return false;
+		// var modal = document.getElementById('modal');
+	});
 });
 // $('.modal-btn').on('click', function(){
 //   var data = $(this).data.val();
