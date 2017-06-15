58efaca7248d10041c61e3e638ea3d89714c8ebf
Mike Fogg <me@mikefogg.co>
2017-06-15 15:59:46 -0400

    Example modal firing thing

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,12 @@
+$(document).ready(function(){
 
-$('.modal-btn').on('click', function(){
-  $.getScript('new.js')
+	// $('.modal-btn').on('click', function(){
+	//   $.getScript('new.js')
+	// });
+
+	$('.create-project').on('click', function(){
+		$.getScript('/projects/new');
+		// Do this at the end so it doesn't actually click the link!
+		return false;
+	})
 });
