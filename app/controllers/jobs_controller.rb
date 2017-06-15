29884d4830d40ea9bd77eebcc3d10673cda4d0f0
tfogg2 <foggts@g.cofc.edu>
2017-06-15 13:45:19 -0400

    Beginning css

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -77,6 +77,6 @@ class JobsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
       # params.fetch(:job, {})
-      params.require(:job).permit(:client, :user_id, :time)
+      params.require(:job).permit(:client, :user_id, :time, :name)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614160006) do
+ActiveRecord::Schema.define(version: 20170615165508) do
 
   create_table "jobs", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170614160006) do
     t.string "client"
     t.integer "time"
     t.integer "user_id"
+    t.string "name"
+    t.string "description"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
