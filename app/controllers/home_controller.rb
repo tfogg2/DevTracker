433147320dd3fec8f5251f1e1ccec1b3cd0c3da2
tfogg2 <foggts@g.cofc.edu>
2017-06-19 15:29:06 -0400

    home redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
 
+  def home
+    @user = current_user
+    if @user
+      if @user.projects.length > 0
+        redirect_to @user
+      else
+
+      end
+    end
+  end
+
 
 end
