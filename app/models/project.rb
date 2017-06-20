0ec73a291c99766397e96588f41cbe3cb98cb69f
tfogg2 <foggts@g.cofc.edu>
2017-06-19 20:24:54 -0400

    progress-bar-mini

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
 	belongs_to :user
 	has_many :steps, dependent: :destroy
+	before_create :set_name
 
 
 
@@ -10,6 +11,12 @@ class Project < ApplicationRecord
 
 	def total_hours
 		hours = self.steps.sum(&:hours)
+	end
+
+	def set_name
+		if ! name
+			self.name = "Project Name"
+		end
 	end
 
 
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,6 +1,6 @@
 class Step < ApplicationRecord
   belongs_to :project
-  before_create :set_status
+  before_create :set_status, :set_hours
   #STATUSES = ['Not Started', 'In Progress', 'Completed']
   STATUSES = ['not_started', 'in_progress', 'completed']
 
@@ -12,6 +12,12 @@ class Step < ApplicationRecord
       self.status = "not_started"
     end
 	end
+
+  def set_hours
+    if ! hours
+      self.hours = 0
+    end
+  end
 
 
 end
