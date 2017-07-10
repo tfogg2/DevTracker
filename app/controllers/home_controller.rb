997629aa0d866c98c3a7469b879ef5679ba7f966
tfogg2 <foggts@g.cofc.edu>
2017-07-10 11:50:47 -0400

    adjustments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def home
     redirect_to user_path(current_user) if current_user
-    set_meta_tags title: "Freelance Dashboard",
-                  keywords: "freelancer, dashboard, projects, client satisfaction, freedash, developer",
+    set_meta_tags title: "Freelancer Dashboard",
+                  keywords: "freelancer, dashboard, projects, client satisfaction, freedash, developer, free",
                   description: "Manage and share your freelance projects from one simple dashboard. Improve client satisfaction, organize and track your freelance projects"
     # && !current_user.projects.empty?
   end
