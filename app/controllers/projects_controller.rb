ed19317f54c0cd6e40d68034a166398ccf1ad79a
tfogg2 <foggts@g.cofc.edu>
2017-07-09 20:40:50 -0400

    adjustments

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -54,7 +54,7 @@ class ProjectsController < ApplicationController
   def destroy
     @project.destroy
     respond_to do |format|
-      format.html { redirect_to user_path(current_user), notice: 'Project was successfully destroyed.' }
+      format.html { redirect_to user_path(current_user), notice: 'Project was successfully deleted.' }
       format.json { head :no_content }
     end
   end
