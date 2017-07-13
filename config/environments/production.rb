38218827db8b8ced8f30730f974afc25cf9ae68a
tfogg2 <foggts@g.cofc.edu>
2017-07-13 18:56:52 -0400

    conversation coming along

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,12 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "devTrack_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  #Cable
+  #wss when https
+  config.action_cable.url = "ws://freedash.io/cable"
+
+  config.action_cable.allowed_request_origins = ['https://freedash.io', 'http://freedash.io']
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
