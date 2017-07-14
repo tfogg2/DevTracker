3190a807fe53798556923f0ec209b2a0b5bde823
tfogg2 <foggts@g.cofc.edu>
2017-07-14 13:11:52 -0400

    middleware

--- config/application.rb
+++ config/application.rb
@@ -15,19 +15,20 @@ module DevTrack
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  end
-  class ConversationActionCable
-    def initialize(app, options={})
-      @app = app
-    end
+    class ConversationActionCable
+      def initialize(app, options={})
+        @app = app
+      end
 
-    def call(env)
-      if Faye::WebSocket.websocket?(env)
-        ActionCable.server.call(env)
-      else
-        @app.call(env)
+      def call(env)
+        if Faye::WebSocket.websocket?(env)
+          ActionCable.server.call(env)
+        else
+          @app.call(env)
+        end
       end
     end
   end
+
 
 end
