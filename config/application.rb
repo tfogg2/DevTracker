6f8ed076049b38ddbb4113bb149be032b6369382
tfogg2 <foggts@g.cofc.edu>
2017-07-14 12:44:54 -0400

    middleware

--- config/application.rb
+++ config/application.rb
@@ -16,17 +16,4 @@ module DevTrack
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-  class ConversationActionCable
-    def initialize(app, options={})
-      @app = app
-    end
-
-    def call(env)
-      if Faye::WebSocket.websocket?(env)
-        ActionCable.server.call(env)
-      else
-        @app.call(env)
-      end
-    end
-  end
 end
