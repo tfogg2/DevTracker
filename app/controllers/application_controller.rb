e5d999f8a93aec5eb51258994079856f4c546ac0
tfogg2 <foggts@g.cofc.edu>
2017-07-13 20:57:20 -0400

    name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-   added_attrs = [ :email, :password, :password_confirmation, :remember_me]
+   added_attrs = [ :email, :password, :password_confirmation, :remember_me, :name]
    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
