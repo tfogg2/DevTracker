c5a54ff000c1bc02bea14eabefef447df0c73af8
tfogg2 <foggts@g.cofc.edu>
2017-07-14 12:46:16 -0400

    middleware

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  require "app/middleware/action_cable_conversation.rb"
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
