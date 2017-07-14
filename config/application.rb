b658eada3c43507a493458f5ad5ce9862582e5ce
tfogg2 <foggts@g.cofc.edu>
2017-07-14 13:24:39 -0400

    middleware

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module DevTrack
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.middleware.use Rack::ConversationActionCable
+    
 
 
     # Settings in config/environments/* take precedence over those specified here.
